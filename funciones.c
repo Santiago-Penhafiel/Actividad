02157d48ad855f32829acca40f0167d97ce62f00
Santiago Peñafiel <santypenafiel8102005@gmail.com>
2024-05-20 18:44:14 -0500

    v2(Primer menu completo)

--- funciones.c
+++
@@ -1,3 +0,0 @@
-#include<stdio.h>
-#include"funciones.h"
-
--- main.c
+++ main.c
@@ -1,9 +1,67 @@
 #include <stdio.h>
 #include <conio.h>
+#include <string.h>
 
 int main (){
+    float datos[100][2];
+    int a, num,cont=0;
+    char nombres[100][20];
+    for (int i = 0; i < 100; i++)
+    {
+          nombres [i][0] = '-';  
+    }
+    
+printf("------------Inventario de objetos------------ \n(Este inventario tiene hasta un máximo de 100 productos para ingresar)\n");
+printf("\t\tMENU \n1 Para ingresar nuevos objetos \n2 Para editar los objetos \n3 Para eliminar los objetos \n4 Para ver la lista de objetos\n");
+    scanf("%d",&a);
+    switch (a)
+    {
+    case 1:
+        printf("Cuantos objetos desea ingresar : ");
+        scanf("%i",&num);
+        while (num<=0 || num>=100)
+        {
+            printf("\nIngrese un numero valido : ");
+            scanf("%i",&num);
+        }
+        
+        for (int i = 0; i < num; i++)
+        {
+            
+            if (strcmp(nombres[cont],"-")==0){
+                printf("Ingrese el nombre del producto %i\n",i+1);
+                scanf("%s",&nombres[cont]);
+                for (int j = 0; j < 2; j++)
+                {
+                    if (j==0)
+                    {
+                        printf("Ingrese la cantidad de productos de %s : ",nombres[cont]);
+                    } else {
+                        printf("Ingrese el precio del producto %s : ",nombres[cont]);
+                    }    
+                    scanf("%f",&datos[cont][j]);
+                }
+            } else {
+                i--;
+            }
+            cont++;
+        }
+        
+        break;
+    
+    case 2:
+        printf("Que numero de producto desea modificar : ");
+        scanf("%i",num);
+        num--;
+        
 
+        break;
+    
+    default:
+        break;
+    }
 
     getch();
+
     return 0;
 }
