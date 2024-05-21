fe04cfe965fce57f885c1576b5c188b44c145034
Santiago Peñafiel <santypenafiel8102005@gmail.com>
2024-05-21 16:42:09 -0500

    Comprobar funcionando

--- main.c
+++ main.c
@@ -2,22 +2,21 @@
 #include <conio.h>
 #include <string.h>
 
+int comprobar (int num,char arreglo[][20]);//verificar array vacio
+
 int main (){
     float datos[100][2];
-    int a, num,cont=0;
-    char nombres[100][20];
-    for (int i = 0; i < 100; i++)
-    {
-          nombres [i][0] = '-';  
-    }
-    
+    int opc, num,cont;
+    char nombres[100][20]={};
+
 printf("------------Inventario de objetos------------ \n(Este inventario tiene hasta un máximo de 100 productos para ingresar)\n");
 inicio:
 printf("\t\tMENU \n1 Para ingresar nuevos objetos \n2 Para editar los objetos \n3 Para eliminar los objetos \n4 Para ver la lista de objetos \n5 Para salir\n");
-    scanf("%d",&a);
-    switch (a)
+    scanf("%d",&opc);
+    switch (opc)
     {
     case 1:
+        cont=0;
         printf("Cuantos objetos desea ingresar : ");
         scanf("%i",&num);
         while (num<=0 || num>=100)
@@ -28,9 +27,9 @@ printf("\t\tMENU \n1 Para ingresar nuevos objetos \n2 Para editar los objetos \n
         
         for (int i = 0; i < num; i++)
         {
-            
-            if (strcmp(nombres[cont],"-")==0){
-                printf("Ingrese el nombre del producto %i\n",i+1);
+            //printf("Vuelta\n");
+            if (comprobar(cont,nombres)==0){
+                printf("Ingrese el nombre del producto %i\n",cont+1);
                 scanf("%s",&nombres[cont]);
                 for (int j = 0; j < 2; j++)
                 {
@@ -47,13 +46,18 @@ printf("\t\tMENU \n1 Para ingresar nuevos objetos \n2 Para editar los objetos \n
             }
             cont++;
         }
-        goto inicio;
+        goto inicio; //reinicio
         break;
     
     case 2:
         printf("Que numero de producto desea modificar : ");
         scanf("%i",&num);
         num--;
+        if (comprobar(num,nombres)==0)
+        {
+            /* code */
+        }
+        
         printf("Nombre\tCantidad\tPrecio\n%s\t%f\t%f\n",nombres[num],datos[num][0],datos[num][1]);
         printf("Nuevo nombre : ");
         scanf("%s",&nombres[num]);
@@ -72,4 +76,20 @@ printf("\t\tMENU \n1 Para ingresar nuevos objetos \n2 Para editar los objetos \n
     getch();
 
     return 0;
+}
+
+
+
+int comprobar (int num,char arreglo[][20]){
+    int c=0;
+    for (int i = 0; i < 20; i++)
+    {
+        if ((arreglo[num][i]) != '\0'){
+            c++;
+            //printf("LLENO\n");
+        } else {
+        //printf("VACIO\n");
+        }
+    }
+    return c;
 }
